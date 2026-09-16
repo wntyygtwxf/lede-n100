529f98213bf009a0fd09c33c06ef8d9927b0a1a6
wntyygtwxf <seasonmusic@gmail.com>
2026-09-16 19:22:52 +0800

    feat(config): align packages with router setup and pre-embed openclash meta core

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 # Description: OpenWrt DIY script part 2 (After Update & Install feeds)
 
-# Set default IP to 192.168.50.1
+# 1. 设置后台默认 IP 为 192.168.50.1
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 
-# Set default theme to Argon
+# 2. 设置默认主题为 Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile || true
+
+# 3. 预置 OpenClash 运行所需的 Mihomo (Clash.Meta) 核心，免去初次开机手动下载核心
+mkdir -p files/etc/openclash/core
+curl -sL https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz -o /tmp/meta.tar.gz
+if [ -s /tmp/meta.tar.gz ]; then
+    tar -zxvf /tmp/meta.tar.gz -C /tmp/
+    chmod +x /tmp/clash
+    mv /tmp/clash files/etc/openclash/core/clash_meta
+    rm -f /tmp/meta.tar.gz
+fi
