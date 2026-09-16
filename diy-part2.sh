2a9bbb77a2cff4d26de841ae8b24fa329cef89ec
karamazov <karamazov@example.com>
2026-09-16 15:41:21 +0800

    feat: change default LAN IP to 192.168.50.1

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Description: OpenWrt DIY script part 2 (After Update & Install feeds)
 
-# Modify default IP (默认 192.168.1.1，如需修改为 192.168.10.1 可解开下行注释)
-# sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# Set default IP to 192.168.50.1
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 
 # Set default theme to Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile || true
