8d211e6ddaa630ffa17d5935f2d2b9fa94babaa4
wntyygtwxf <seasonmusic@gmail.com>
2026-09-17 20:26:07 +0800

    refactor: use clean upstream lean master branch without version spoofing

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-# 确保 LuCI feed 明确使用 openwrt-25.12 分支
-sed -i 's/^#\(.*openwrt-25.12\)/\1/' feeds.conf.default
-sed -i 's/^src-git luci .*openwrt-24.10/#&/' feeds.conf.default || true
-sed -i 's/^src-git luci .*openwrt-23.05/#&/' feeds.conf.default || true
+# Uncomment a feed source or add custom feed if needed
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
